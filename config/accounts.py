f0e8636629e2bcf69b1c2590be712acb39b44d80
bibletalks1612-glitch <bibletalks1612@gmail.com>
2026-05-16 10:41:40 +0530

    Add multi-account configuration template

---
+++ config/accounts.py
@@ -0,0 +1,20 @@
+# ForexMind Multi-Account Configuration
+# Add your accounts here
+
+ACCOUNTS = [
+    {
+        "login": 10010854151,
+        "password": "your_demo_password",
+        "server": "MetaQuotes-Demo",
+        "mode": "demo",
+        "name": "Demo Account"
+    },
+    # Uncomment to add live account
+    # {
+    #     "login": 12345678,
+    #     "password": "your_live_password",
+    #     "server": "Your-Broker-Server",
+    #     "mode": "live",
+    #     "name": "Live Account"
+    # },
+]
